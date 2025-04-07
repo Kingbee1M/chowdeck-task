d0aff29f7dda086710ee06291cfeb354c140a5a1
HENRY ODUNSI <n.rytech15@gmail.com>
2025-04-07 09:53:13 +0100

    structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,83 +1,93 @@
-import { View, Image, StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, Platform, Pressable,TextInput } from 'react-native';
-import Header from '@/components/Header';
-import Services from '@/components/Services';
-import ImageSlider from '@/components/ImageSlider';
-import Explore from '@/components/Explore';
-const restaurantImg = require('@/assets/images/restaurant-building.png');
-const supermarketImg = require('@/assets/images/super-market.png');
-const pharmacyImg = require('@/assets/images/pharmacy.png');
-const deliveryImg = require('@/assets/images/delivery-truck.png');
-const marketImg = require('@/assets/images/crate.png');
-const moreImg = require('@/assets/images/coffee-cup.png');
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Platform,
+  Pressable,
+  TextInput,
+} from "react-native";
+import Header from "@/components/Header";
+import Services from "@/components/Services";
+import ImageSlider from "@/components/ImageSlider";
+import Explore from "@/components/Explore";
+const restaurantImg = require("@/assets/images/restaurant-building.png");
+const supermarketImg = require("@/assets/images/super-market.png");
+const pharmacyImg = require("@/assets/images/pharmacy.png");
+const deliveryImg = require("@/assets/images/delivery-truck.png");
+const marketImg = require("@/assets/images/crate.png");
+const moreImg = require("@/assets/images/coffee-cup.png");
 
 const services = [
   {
     name: "Restaurant",
     bgColor: "#FFC0CB",
-    img: restaurantImg
+    img: restaurantImg,
   },
   {
     name: "Supermarket",
     bgColor: "#FFB6C1",
-    img: supermarketImg
+    img: supermarketImg,
   },
   {
     name: "Pharmacies",
     bgColor: "#87CEEB",
-    img: pharmacyImg
+    img: pharmacyImg,
   },
   {
     name: "Send Packages",
     bgColor: "#FFD1DC",
-    img: deliveryImg
+    img: deliveryImg,
   },
   {
     name: "Local Market",
     bgColor: "#ADD8E6",
-    img: marketImg
+    img: marketImg,
   },
   {
     name: "More",
     bgColor: "#B0E0E6",
-    img: moreImg
+    img: moreImg,
   },
 ];
 
 const sliderImages = [
-  require('@/assets/images/chicken-skewers-with-slices-sweet-peppers-dill.jpg'),
-  require('@/assets/images/chicken-skewers-with-onions-top-salad.jpg'),
-  require('@/assets/images/chicken-chop-french-fries-wooden-board.jpg'),
+  require("@/assets/images/chicken-skewers-with-slices-sweet-peppers-dill.jpg"),
+  require("@/assets/images/chicken-skewers-with-onions-top-salad.jpg"),
+  require("@/assets/images/chicken-chop-french-fries-wooden-board.jpg"),
 ];
 
 const sliderImages2 = [
-  require('@/assets/images/chicken-chop-french-fries-wooden-board.jpg'),
-  require('@/assets/images/stewed-white-beans-sliced-pumpkin-tomato-sauce.jpg'),
-  require('@/assets/images/chicken-skewers-with-onions-top-salad.jpg'),
+  require("@/assets/images/chicken-chop-french-fries-wooden-board.jpg"),
+  require("@/assets/images/stewed-white-beans-sliced-pumpkin-tomato-sauce.jpg"),
+  require("@/assets/images/chicken-skewers-with-onions-top-salad.jpg"),
 ];
 
 export default function HomeScreen() {
-
   return (
     <SafeAreaView style={styles.safe}>
-      <ScrollView 
-        style={styles.scrollView} 
+      <ScrollView
+        style={styles.scrollView}
         contentContainerStyle={styles.scrollViewContent}
       >
         <View style={styles.wrapper}>
-        <Header />
-        <ImageSlider images={sliderImages} />
-        <View style={styles.serviceContainer}>
-        {services.map((service, index) => (
-              <Services 
-                key={index} 
-                name={service.name} 
-                img={service.img} 
-                color={service.bgColor} 
+          <Header />
+          <ImageSlider images={sliderImages} />
+          <View style={styles.serviceContainer}>
+            {services.map((service, index) => (
+              <Services
+                key={index}
+                name={service.name}
+                img={service.img}
+                color={service.bgColor}
               />
             ))}
-        </View>
-        <ImageSlider images={sliderImages2} />
-        <Explore />
+          </View>
+          <ImageSlider images={sliderImages2} />
+          <Explore />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -86,16 +96,16 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   safe: {
-    flex: 1
+    flex: 1,
   },
   scrollView: {
-    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flexGrow: 1,
   },
   scrollViewContent: {
     flexGrow: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   wrapper: {
     width: "80%",
@@ -106,7 +116,7 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "20%",
-    marginTop: 20
+    marginTop: 20,
   },
   serviceContainer: {
     width: "100%",
@@ -118,5 +128,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignContent: "center",
     marginTop: 20,
-  }
+  },
 });
